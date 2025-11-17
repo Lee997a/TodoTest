035b84a1225dc3c3ef5e08d1a2621c494c29cd75
Lee997a <ehddn5476@gmail.com>
2025-11-17 14:13:58 +0900

    ip 변경

--- Backend/src/main/java/com/example/Backend/config/WebConfig.java
+++ Backend/src/main/java/com/example/Backend/config/WebConfig.java
@@ -10,7 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000",
+                        "http://192.168.0.213:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
